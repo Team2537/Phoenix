f862301f03908864e2fb22ee8c89580d290a1684
Space Raiders <spaceraidersahs2537@gmail.com>
2018-02-15 16:20:57 -0500

    pre-test vert code

--- Phoenix/src/org/usfirst/frc/team2537/robot/vert/VertSubsystem.java
+++ Phoenix/src/org/usfirst/frc/team2537/robot/vert/VertSubsystem.java
@@ -2,23 +2,28 @@ package org.usfirst.frc.team2537.robot.vert;
 
 import org.usfirst.frc.team2537.robot.Ports;
 import org.usfirst.frc.team2537.robot.input.HumanInput;
+import org.usfirst.frc.team2537.robot.resources.CANTalon;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class VertSubsystem extends Subsystem {
 
-	private Talon vertMotorOne;
-	private Talon vertMotorTwo;
-	private DigitalInput limitSwitch;
+	private CANTalon vertMotorOne;
+	private CANTalon vertMotorTwo;
 	double current;
 
 	public VertSubsystem() { 
-		vertMotorOne = new Talon(Ports.VERT_MOTOR_ONE);
-		vertMotorTwo = new Talon(Ports.VERT_MOTOR_TWO);
-		limitSwitch = new DigitalInput(Ports.VERT_LIMIT_SWITCH);
+		vertMotorOne = new CANTalon(Ports.VERT_MOTOR_ONE);
+		vertMotorTwo = new CANTalon(Ports.VERT_MOTOR_TWO);
+		
+		vertMotorOne.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
+		
+		vertMotorTwo.setControlMode(ControlMode.Follower);
+		vertMotorTwo.setInverted(true);
 	}
 
 	public void initDefaultCommand() {
@@ -34,11 +39,8 @@ public class VertSubsystem extends Subsystem {
 	public void setVertMotors(double speed) {
 		// CHECK THAT THESE ARE CORRECT BEFORE STARTING TO AVOID DESTROYING GEARBOX
 		vertMotorOne.set(speed);
-		vertMotorTwo.set(speed);
 	}
 	
-	public boolean getLimitSwitch() {
-		return limitSwitch.get();
-	}
+
 
 }
