f37167dc1df20d399516ce0cfda425abcdb89cbe
Arden Zhang <arden.zhang@gmail.com>
2018-02-24 11:42:48 -0500

    So much auto. Nowhere near done.

--- Phoenix/src/org/usfirst/frc/team2537/robot/auto/AutoChooser.java
+++ Phoenix/src/org/usfirst/frc/team2537/robot/auto/AutoChooser.java
@@ -1,13 +1,17 @@
 package org.usfirst.frc.team2537.robot.auto;
 
-import org.usfirst.frc.team2537.robot.auto.routes.LeftAuto;
-import org.usfirst.frc.team2537.robot.auto.routes.RightAuto;
+import org.usfirst.frc.team2537.robot.auto.routes.LeftCoopAuto;
+import org.usfirst.frc.team2537.robot.auto.routes.LeftSoloAuto;
+import org.usfirst.frc.team2537.robot.auto.routes.RightCoopAuto;
+import org.usfirst.frc.team2537.robot.auto.routes.RightSoloAuto;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 public class AutoChooser extends SendableChooser<Command> {
 	public AutoChooser() {
-		addObject("Left", new LeftAuto());
-		addObject("Right", new RightAuto());
+		addObject("Left Solo", new LeftSoloAuto());
+		addObject("Left With Teammate", new LeftCoopAuto());
+		addObject("Right Solo", new RightSoloAuto());
+		addObject("Right With Teammate", new RightCoopAuto());
 	}
 }
